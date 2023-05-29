6d0ddba23a7d094fafd1459437672cdece75f2a9
javierrm11 <108242503+javierrm11@users.noreply.github.com>
2023-05-29 12:45:10 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// Obtener una referencia al elemento canvas del DOM
+const $grafica = document.querySelector("#graph");
+// Las etiquetas son las que van en el eje X. 
+const etiquetas = ["Enero", "Febrero", "Marzo", "Abril"]
+// Podemos tener varios conjuntos de datos
+const datosVentas2020 = {
+    data: [5000, 1500, 8000, 5102], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+const datosVentas2021 = {
+    data: [10000, 1700, 5000, 5989], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(255, 159, 64, 0.2)',// Color de fondo
+    borderColor: 'rgba(255, 159, 64, 1)',// Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+
+new Chart($grafica, {
+    type: 'line',// Tipo de gráfica
+    data: {
+        labels: etiquetas,
+        datasets: [
+            datosVentas2020,
+            datosVentas2021,
+            // Aquí más datos...
+        ]
+    },
+    options: {
+      legend: {
+         display: false,
+           labels: {
+             display: false
+           }
+       },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        },
+        
+    }
+});
