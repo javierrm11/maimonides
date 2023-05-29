10dd1f13aa23e2264f4e2945727da746fb1a80a9
javierrm11 <108242503+javierrm11@users.noreply.github.com>
2023-05-29 12:47:25 +0200

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,38 @@
 // Obtener una referencia al elemento canvas del DOM
-const $grafica = document.querySelector("#grafica");
+const $grafica = document.querySelector("#graph");
 // Las etiquetas son las que van en el eje X. 
 const etiquetas = ["Enero", "Febrero", "Marzo", "Abril"]
-// Podemos tener varios conjuntos de datos. Comencemos con uno
+// Podemos tener varios conjuntos de datos
 const datosVentas2020 = {
-    label: "Ventas por mes",
     data: [5000, 1500, 8000, 5102], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-    backgroundColor: '#d0d55d', // Color de fondo
-    borderColor: '#d0d55d', // Color del borde
+    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
     borderWidth: 1,// Ancho del borde
-    barPercentage: 0.4
 };
+const datosVentas2021 = {
+    data: [10000, 1700, 5000, 5989], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(255, 159, 64, 0.2)',// Color de fondo
+    borderColor: 'rgba(255, 159, 64, 1)',// Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+
 new Chart($grafica, {
-    type: 'bar',// Tipo de gráfica
+    type: 'line',// Tipo de gráfica
     data: {
         labels: etiquetas,
         datasets: [
             datosVentas2020,
+            datosVentas2021,
             // Aquí más datos...
         ]
     },
     options: {
+      legend: {
+         display: false,
+           labels: {
+             display: false
+           }
+       },
         scales: {
             yAxes: [{
                 ticks: {
@@ -28,6 +40,6 @@ new Chart($grafica, {
                 }
             }],
         },
+        
     }
-    
 });
